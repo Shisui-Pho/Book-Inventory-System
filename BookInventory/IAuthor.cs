bcfe251b2367ab035ecbb8f8bf29a8945b02f935
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-09-08 18:25:30 +0200

    Added interfaces for the book and author models

---
+++ BookInventory/IAuthor.cs
@@ -0,0 +1,12 @@
+﻿/*
+ * This file contains an interface that defines the basic structure of the Author model
+ */
+namespace BookInventory
+{
+    public interface IAuthor
+    {
+        string Name { get; set; }
+        string Surname { get; set; }
+        int NumberOfPublishedBooks { get; }
+    }//class
+}//namespace
---
+++ BookInventory/IBook.cs
@@ -0,0 +1,13 @@
+﻿/*
+ * This file contains an interface that defines the basic structure of the Book model
+ */
+namespace BookInventory
+{
+    public interface IBook
+    {
+        int ID { get; }
+        string ISBN { get; }
+        IAuthor BookAuthor { get; }
+        string Genre { get; }
+    }//class
+}//namespace
