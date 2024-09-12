a6adb4da0a151e50eb6eebe762c6c121b171ed31
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-09-12 21:25:33 +0200

    Added a concrete implemantation of the IAuther interface

---
+++ BookInventory/Author.cs
@@ -0,0 +1,26 @@
+﻿/*
+ *  This file contains the concrete implementation of the IAuthor interface
+ */
+namespace BookInventory
+{
+    public class Author : IAuthor
+    {
+        public int ID { get; internal set; } = default;
+
+        public string Name { get; private set; }
+
+        public string Surname { get; private set; }
+
+        public int NumberOfPublishedBooks { get; internal set; }
+        public Author(string name, string surname)
+        {
+            this.Name = name;
+            this.Surname = surname;
+        }//ctor main
+        internal Author(string name, string surname, int NumberOfPublishedBooks)
+            : this(name, surname)
+        {
+            this.NumberOfPublishedBooks = NumberOfPublishedBooks;
+        }//ctor 2
+    }//class
+}//namespace
