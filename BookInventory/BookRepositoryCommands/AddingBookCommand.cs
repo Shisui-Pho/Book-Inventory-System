f4c07b52eea061a8579f93c95f7d48d8153b4b52
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-09-21 14:20:12 +0200

    Added a commanding for adding a new book to the database

---
+++ BookInventory/BookRepositoryCommands/AddingBookCommand.cs
@@ -0,0 +1,10 @@
+﻿/*
+ * This file contains the AddBook Command class that will handle a add a new book to the database
+ */
+namespace BookInventory
+{
+    internal class AddingBookCommand
+    {
+
+    }//class
+}//namespace
