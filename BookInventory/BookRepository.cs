0dcfdb7a70e3e81e6d30df5db931c8f1fc244b96
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-09-21 12:34:18 +0200

    Implemented the filter method by parameters

--- BookInventory/BookRepository.cs
+++ BookInventory/BookRepository.cs
@@ -145,10 +145,77 @@ namespace BookInventory
                 return false;
             }//end catch
         }//AddAuthors
-        public IEnumerable<IBook> FilterBooks(string authorName = null, string genre = null, int? quantity = null, string title = null, int? release = null)
+        public IEnumerable<IBook> FilterBooks(string authorName = null, string genre = null, string title = null, int? release = null)
         {
-            
+            List<IBook> books = new List<IBook>();
+            string _sql = BuildSql(authorName, genre, title, release);
+            try
+            {
+                if (_dbService.GetConnection().State == ConnectionState.Closed)
+                    _dbService.GetConnection().Open();
+
+                OleDbCommand cmd = new OleDbCommand(_sql, _dbService.GetConnection());
+                OleDbDataReader rd = cmd.ExecuteReader();//Read the filtered data
+
+                while (rd.Read())
+                {
+                    int bookid = int.Parse(rd["Book_ID"].ToString());
+                    string isbn = rd["Book_ISBN"].ToString();
+                    genre = rd["Genre"].ToString();
+                    title = rd["Book_Title"].ToString();
+                    int quantity = int.Parse(rd["Quantity"].ToString());
+                    int publication = int.Parse(rd["PublicationYear"].ToString());
+
+                    //Get the book authors
+                    IEnumerable<IAuthor> authors = GetAuthors(bookid);
+
+                    Book book = new Book(isbn, title, genre, authors, publication, quantity);
+                    book.ID = bookid;
+
+                    books.Add(book);
+                }
+            }//end try
+            catch
+            (Exception ex)
+            {
+                ExceptionLogger.GetLogger().LogError(ex);
+            }
+            finally
+            {
+                _dbService.GetConnection().Close();
+            }
+            return books;
         }//FilterBooks
+        private string BuildSql(string authorName, string genre, string title, int? release)
+        {
+            string sql_ = " SELECT Book.Book_ID, Book.Book_Title, Book.Book_ISBN, Book.PublicationYear, Book.Genre, Book.Quantity" +
+                          " FROM ( Book INNER JOIN BookAuthor ON Book.Book_ID =  BookAuthor.Book_ID)" +
+                          " INNER JOIN Author ON BookAuthor.Author_ID = Author.Author_ID ";
+            
+            //A queue for the query commands
+            Queue<string> qCriteria = new Queue<string>();
+            if (!String.IsNullOrEmpty(authorName))
+                qCriteria.Enqueue(String.Format(" (Author.Author_Name LIKE '*{0}*' OR Author.Author_Surname LIKE '*{0}*')", authorName));
+            if (!String.IsNullOrEmpty(genre))
+                qCriteria.Enqueue(String.Format(" Book.Genre LIKE \'*{0}*\'", genre));
+            if (!String.IsNullOrEmpty(title))
+                qCriteria.Enqueue(String.Format(" Book.Book_Title LIKE \'*{0}*\'", title));
+            if (release.HasValue)
+                qCriteria.Enqueue(String.Format(" Book.PublicationYear = {0} ", release));
+
+            bool isFirstCondition = true;
+            while(qCriteria.Count > 0)
+            {
+                if (isFirstCondition)
+                {
+                    sql_ += " WHERE " + qCriteria.Dequeue();
+                    isFirstCondition = false;
+                }
+                else
+                    sql_ += " AND " + qCriteria.Dequeue();
+            }//end while
+            return sql_;
+        }//Build Sql method
         public IEnumerable<IBook> FilterBooks(Predicate<IBook> predicate)
         {
             foreach (IBook book in LoadAllBooks())
@@ -329,7 +396,6 @@ namespace BookInventory
 
             return isTransactionComplete;
         }//UpdateBook
-
         #endregion Completed Methods
         public bool RemoveBook(IBook book)
         {
