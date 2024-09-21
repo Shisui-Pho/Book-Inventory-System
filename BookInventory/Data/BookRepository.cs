d6df471bc659ab4b50de98933c563160814c36d9
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-09-21 14:54:12 +0200

    Refactored the BookRepository to mkae use of the Commands

--- BookInventory/Data/BookRepository.cs
+++ BookInventory/Data/BookRepository.cs
@@ -14,74 +14,44 @@ namespace BookInventory
         {
             this._dbService = service;
         }
-        #region Completed Methods
         public bool AddBook(IBook book)
         {
-            IDbTransaction trans = null;
-            bool isTransactionComplete = false;
-            try
-            {
-                if (_dbService.GetConnection().State == ConnectionState.Closed)
-                    _dbService.GetConnection().Open();
-                //Begin a transaction
-                trans = _dbService.GetConnection().BeginTransaction();
-
-                #region Adding the book
-                string _sql = "qAddBook";
-                OleDbCommand cmdAddBook = new OleDbCommand(_sql, _dbService.GetConnection(), (OleDbTransaction)trans);
-
-                //Pass all book data as parameters
-                cmdAddBook.Parameters.AddWithValue("@isbn", book.ISBN);
-                cmdAddBook.Parameters.AddWithValue("@title", book.Title);
-                cmdAddBook.Parameters.AddWithValue("@publicationYear", book.PublicationYear);
-                cmdAddBook.Parameters.AddWithValue("@quantity", book.Quantity);
-                cmdAddBook.Parameters.AddWithValue("@genre", book.Genre);
-
-                //Configure the command to execute a stored procedure
-                cmdAddBook.CommandType = CommandType.StoredProcedure;
-                
-                //Insert data
-                int _status = cmdAddBook.ExecuteNonQuery();
-
-                #endregion Adding the book
-
-                //-Check if there was a change or not
-                if (_status == 0)
-                    return isTransactionComplete;
-
-                 //Successfuly inserted
-                 //-Get the ID of the book
-                _sql = "SELECT LAST(Book_ID) FROM Book";
-                cmdAddBook = new OleDbCommand(_sql, _dbService.GetConnection(), (OleDbTransaction)trans);
-                int bookid = (int)cmdAddBook.ExecuteScalar();
-
-                //Update the book id
-                ((Book)book).ID = bookid;
-
-                isTransactionComplete = AddAuthors(book, trans);
-                if (isTransactionComplete)
-                    trans.Commit();//Save the changes
-
-                return isTransactionComplete;
-            }//end try
-            catch
-            (Exception ex)
-            {
-                //Log error
-                ExceptionLogger.GetLogger().LogError(ex);
-                return false;
-            }
-            finally
-            {
-                if (!isTransactionComplete && trans != null)
-                    trans.Rollback();
-
-                if (trans != null)
-                    trans.Dispose();
-                trans?.Dispose();
-                _dbService.GetConnection().Close();
-            }//end finally
+            AddingBookCommand cmd = new AddingBookCommand(_dbService, AddAuthors);//Create the command
+            return cmd.AddBook(book);//Add the book
         }//AddBook
+        
+        public IEnumerable<IBook> FilterBooks(string authorName = null, string genre = null, string title = null, int? release = null)
+        {
+            FilterBooksCommand cmd = new FilterBooksCommand(_dbService, GetAuthors);//Create the command
+            return cmd.FilterBooks(authorName, genre, title, release);
+        }//FilterBooks
+        public IEnumerable<IBook> FilterBooks(Predicate<IBook> predicate)
+        {
+            FilterBooksCommand cmd = new FilterBooksCommand(_dbService, GetAuthors);//Create the command
+            return cmd.FilterBooks(predicate);
+        }//FilterBooks
+        public IBook FindByISBN(string isbn)
+        {
+            LoadingBooksCommand cmd = new LoadingBooksCommand(_dbService, GetAuthors);
+            return cmd.FindByISBN(isbn);
+        }//FindByISBN
+        public IEnumerable<IBook> LoadAllBooks()
+        {
+            LoadingBooksCommand cmd = new LoadingBooksCommand(_dbService, GetAuthors);
+            return cmd.LoadAllBooks();
+        }//LoadAllBooks
+        public bool UpdateBook(IBook book)
+        {
+            UpdateBookCommand cmd = new UpdateBookCommand(_dbService, AddAuthors);
+            return cmd.UpdateBook(book);
+        }//UpdateBook
+        public bool RemoveBook(IBook book)
+        {
+            RemoveBookCommand cmd = new RemoveBookCommand(_dbService);
+            return cmd.RemoveBook(book);
+        }//RemoveBook
+
+        #region Delegate helper methods
         private bool AddAuthors(IBook book, IDbTransaction transaction)
         {
             try
@@ -106,7 +76,7 @@ namespace BookInventory
 
                         if (_status == 0)
                             return false; //If nothing was added it means that something wrong has happened
-                        
+
                         //Configure command for getting the author id
                         cmd.CommandText = "SELECT LAST(Author_ID) FROM Author";
                         cmd.CommandType = CommandType.Text;
@@ -147,83 +117,6 @@ namespace BookInventory
                 return false;
             }//end catch
         }//AddAuthors
-        public IEnumerable<IBook> FilterBooks(string authorName = null, string genre = null, string title = null, int? release = null)
-        {
-            List<IBook> books = new List<IBook>();
-            string _sql = BuildSql(authorName, genre, title, release);
-            try
-            {
-                if (_dbService.GetConnection().State == ConnectionState.Closed)
-                    _dbService.GetConnection().Open();
-
-                OleDbCommand cmd = new OleDbCommand(_sql, _dbService.GetConnection());
-                OleDbDataReader rd = cmd.ExecuteReader();//Read the filtered data
-
-                while (rd.Read())
-                {
-                    int bookid = int.Parse(rd["Book_ID"].ToString());
-                    string isbn = rd["Book_ISBN"].ToString();
-                    genre = rd["Genre"].ToString();
-                    title = rd["Book_Title"].ToString();
-                    int quantity = int.Parse(rd["Quantity"].ToString());
-                    int publication = int.Parse(rd["PublicationYear"].ToString());
-
-                    //Get the book authors
-                    IEnumerable<IAuthor> authors = GetAuthors(bookid);
-
-                    Book book = new Book(isbn, title, genre, authors, publication, quantity);
-                    book.ID = bookid;
-
-                    books.Add(book);
-                }
-            }//end try
-            catch
-            (Exception ex)
-            {
-                ExceptionLogger.GetLogger().LogError(ex);
-            }
-            finally
-            {
-                _dbService.GetConnection().Close();
-            }
-            return books;
-        }//FilterBooks
-        private string BuildSql(string authorName, string genre, string title, int? release)
-        {
-            string sql_ = " SELECT Book.Book_ID, Book.Book_Title, Book.Book_ISBN, Book.PublicationYear, Book.Genre, Book.Quantity" +
-                          " FROM ( Book INNER JOIN BookAuthor ON Book.Book_ID =  BookAuthor.Book_ID)" +
-                          " INNER JOIN Author ON BookAuthor.Author_ID = Author.Author_ID ";
-            
-            //A queue for the query commands
-            Queue<string> qCriteria = new Queue<string>();
-            if (!String.IsNullOrEmpty(authorName))
-                qCriteria.Enqueue(String.Format(" (Author.Author_Name LIKE '*{0}*' OR Author.Author_Surname LIKE '*{0}*')", authorName));
-            if (!String.IsNullOrEmpty(genre))
-                qCriteria.Enqueue(String.Format(" Book.Genre LIKE \'*{0}*\'", genre));
-            if (!String.IsNullOrEmpty(title))
-                qCriteria.Enqueue(String.Format(" Book.Book_Title LIKE \'*{0}*\'", title));
-            if (release.HasValue)
-                qCriteria.Enqueue(String.Format(" Book.PublicationYear = {0} ", release));
-
-            bool isFirstCondition = true;
-            while(qCriteria.Count > 0)
-            {
-                if (isFirstCondition)
-                {
-                    sql_ += " WHERE " + qCriteria.Dequeue();
-                    isFirstCondition = false;
-                }
-                else
-                    sql_ += " AND " + qCriteria.Dequeue();
-            }//end while
-            return sql_;
-        }//Build Sql method
-        public IEnumerable<IBook> FilterBooks(Predicate<IBook> predicate)
-        {
-            foreach (IBook book in LoadAllBooks())
-                if (predicate(book))
-                    yield return book;
-        }//FilterBooks
         private IEnumerable<IAuthor> GetAuthors(int bookid)
         {
             IList<IAuthor> authors = new List<IAuthor>();
@@ -257,200 +150,7 @@ namespace BookInventory
             }
             return authors;
         }
-        public IBook FindByISBN(string isbn)
-        {
-            IBook book = null;
-            try
-            {
-                if (_dbService.GetConnection().State == ConnectionState.Closed)
-                    _dbService.GetConnection().Open();
-                //_dbService.GetConnection().Open();
-                string sql = "qGetBookByISBN";
-                OleDbCommand cmd = new OleDbCommand(sql, _dbService.GetConnection());
-                cmd.Parameters.AddWithValue("@isbn", isbn);
-                cmd.CommandType = CommandType.StoredProcedure;
-                OleDbDataReader rd = cmd.ExecuteReader();
 
-                while (rd.Read())
-                {
-                    string genre = rd["Genre"].ToString();
-                    string title = rd["Book_Title"].ToString();
-                    int quantity = int.Parse(rd["Quantity"].ToString());
-                    int publication = int.Parse(rd["PublicationYear"].ToString());
-                    int bookid = int.Parse(rd["Book_ID"].ToString());
-
-                    //Get the book authors and their information
-                    IEnumerable<IAuthor> authors = GetAuthors(bookid);
-
-                    book = new Book(isbn, title, genre, authors, publication, quantity);
-                    ((Book)book).ID = bookid;
-                    break;//Only return the first instance of the book
-                }
-            }
-            catch
-            (Exception ex)
-            {
-                ExceptionLogger.GetLogger().LogError(ex);
-            }
-            finally
-            {
-                _dbService.GetConnection().Close();
-            }
-
-            return book;
-        }//FindByISBN
-        public IEnumerable<IBook> LoadAllBooks()
-        {
-            List<IBook> books = new List<IBook>();
-            try
-            {
-                if (_dbService.GetConnection().State == ConnectionState.Closed)
-                    _dbService.GetConnection().Open();
-
-                string sql = "qBookDetails";
-                OleDbCommand cmd = new OleDbCommand(sql, _dbService.GetConnection());
-                cmd.CommandType = CommandType.StoredProcedure;
-
-                OleDbDataReader rd = cmd.ExecuteReader();
-
-                while (rd.Read())
-                {
-                    int bookid = int.Parse(rd["Book_ID"].ToString());
-                    string isbn = rd["Book_ISBN"].ToString();
-                    string genre = rd["Genre"].ToString();
-                    string title = rd["Book_Title"].ToString();
-                    int quantity = int.Parse(rd["Quantity"].ToString());
-                    int publication = int.Parse(rd["PublicationYear"].ToString());
-
-                    //Get the book authors
-                    IEnumerable<IAuthor> authors = GetAuthors(bookid);
-
-                    Book book = new Book(isbn, title, genre, authors, publication, quantity);
-                    book.ID = bookid;
-
-                    books.Add(book);
-                }//end while
-            }
-            catch
-            (Exception ex)
-            {
-                ExceptionLogger.GetLogger().LogError(ex);
-                return new List<IBook>();
-            }
-            finally
-            {
-                _dbService.GetConnection().Close();
-            }
-            return books;
-        }//LoadAllBooks
-        public bool UpdateBook(IBook book)
-        {
-            IDbTransaction trans = null;
-            bool isTransactionComplete = false;
-            try
-            {
-                if (_dbService.GetConnection().State == ConnectionState.Closed)
-                    _dbService.GetConnection().Open();
-
-                trans = _dbService.GetConnection().BeginTransaction();
-
-                //Update book details first
-                string sql = "qUpdateBook";
-
-                OleDbCommand cmd = new OleDbCommand(sql, _dbService.GetConnection(), (OleDbTransaction)trans);
-                cmd.Parameters.AddWithValue("@title", book.Title);
-                cmd.Parameters.AddWithValue("@genre", book.Genre);
-                cmd.Parameters.AddWithValue("@isbn", book.ISBN);
-                cmd.Parameters.AddWithValue("@quantity", book.Quantity);
-                cmd.Parameters.AddWithValue("@publication", book.PublicationYear);
-                cmd.Parameters.AddWithValue("@bookid", book.ID);
-                cmd.CommandType = CommandType.StoredProcedure;
-                int status = cmd.ExecuteNonQuery();
-
-                if (status == 0)
-                    return isTransactionComplete;
-                //Also check the authors
-                isTransactionComplete = AddAuthors(book, trans);
-
-                if (isTransactionComplete)
-                    trans.Commit();
-            }
-            catch
-            (Exception ex)
-            {
-                _ = ExceptionLogger.GetLogger().LogError(ex);
-                return false;
-            }
-            finally
-            {
-                if (trans != null)
-                {
-                    if (isTransactionComplete)
-                        trans.Commit();
-                    else
-                        trans.Rollback();
-
-                    trans.Dispose();
-                }//end if
-
-                _dbService.GetConnection().Close();
-            }//end finally
-
-            return isTransactionComplete;
-        }//UpdateBook
-        #endregion Completed Methods
-        public bool RemoveBook(IBook book)
-        {
-            OleDbTransaction trans = null;
-            if (book == null || book.ID == default)
-                return false;
-            bool isTransactionComplete = false;
-            try
-            {
-                if (_dbService.GetConnection().State == ConnectionState.Closed)
-                    _dbService.GetConnection().Open();
-
-                trans = _dbService.GetConnection().BeginTransaction();
-
-                OleDbCommand cmd = new OleDbCommand();
-                cmd.Connection = _dbService.GetConnection();
-                cmd.Transaction = trans;
-                cmd.CommandText = "qDeleteBookAuthor";
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@bookid", book.ID);
-
-                //Remove the link between book and author
-                _ = cmd.ExecuteNonQuery();
-
-                cmd.CommandText = "qDeleteBook";
-
-                //Remove the book
-                _ = cmd.ExecuteNonQuery();
-
-                //Commit the chages
-                trans.Commit();
-                isTransactionComplete = true;
-            }
-            catch
-            (Exception ex)
-            {
-                //Log error
-                ExceptionLogger.GetLogger().LogError(ex); 
-            }
-            finally
-            {
-                if(trans != null)
-                {
-                    if (isTransactionComplete)
-                        trans.Commit();
-                    else
-                        trans.Rollback();
-                    trans.Dispose();
-                }//end 
-                _dbService.GetConnection().Close();
-            }//end finally
-
-            return isTransactionComplete;
-        }//RemoveBook
+        #endregion Delegate helper methods
     }//class
 }//namespace
