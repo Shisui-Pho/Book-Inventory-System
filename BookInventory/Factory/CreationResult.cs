2be5be7ae6fc5577e98e58ac541dc438df0c1895
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-09-24 21:34:52 +0200

    Implement ICreationResult interface and CreationResult class to handle object creation results

---
+++ BookInventory/Factory/CreationResult.cs
@@ -0,0 +1,21 @@
+﻿/*
+ *  This file contains the concrete implementation of ICreationResult that will be returned to the user upon creation of the object with details
+ */
+namespace BookInventory
+{
+    public class CreationResult<T> : ICreationResult<T>
+    {
+        public string Message { get; private set; }
+
+        public T Item { get; private set; }
+
+        public bool CreationSuccessful {get; private set;}
+
+        public CreationResult(string message, T item, bool isSuccessful)
+        {
+            this.Message = message;
+            this.Item = item;
+            this.CreationSuccessful = isSuccessful;
+        }//ctor main
+    }//class
+}//namespace
