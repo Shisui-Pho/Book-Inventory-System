92837ede89d705cd3bf4f6455a551fb1276d5ae7
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-09-30 03:03:14 +0200

    Added an enum that will define the different types of databases

---
+++ BookInventory/Factory/DatabaseType.cs
@@ -0,0 +1,11 @@
+﻿/*
+ *  This file contains the enum that defines the different types of databases that can be used
+ */
+namespace BookInventory
+{
+    public enum DatabaseType
+    {
+        AccessDB, 
+        SQLServerDB
+    }//enum
+}//namespace
