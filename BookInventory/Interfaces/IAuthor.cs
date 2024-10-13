355cfa0ee69d202e36e9c9f11add4ec675aecec0
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-10-13 13:41:39 +0200

    Updated the author model to reflact the database author table.

--- BookInventory/Interfaces/IAuthor.cs
+++ BookInventory/Interfaces/IAuthor.cs
@@ -1,6 +1,7 @@
 ﻿/*
  * This file contains an interface that defines the basic structure of the Author model
  */
+using System;
 namespace BookInventory
 {
     public interface IAuthor
@@ -8,6 +9,7 @@ namespace BookInventory
         int ID { get; }
         string Name { get; }
         string Surname { get; }
+        DateTime DOB { get; }
         int NumberOfPublishedBooks { get; }
     }//class
 }//namespace
--- BookInventory/Models/Author.cs
+++ BookInventory/Models/Author.cs
@@ -1,6 +1,8 @@
 ﻿/*
  *  This file contains the concrete implementation of the IAuthor interface
  */
+using System;
+
 namespace BookInventory
 {
     public class Author : IAuthor
@@ -12,13 +14,17 @@ namespace BookInventory
         public string Surname { get; private set; }
 
         public int NumberOfPublishedBooks { get; internal set; }
-        public Author(string name, string surname)
+
+        public DateTime DOB { get; private set; }
+
+        public Author(string name, string surname, DateTime dob)
         {
             this.Name = name;
             this.Surname = surname;
+            this.DOB = dob;
         }//ctor main
-        internal Author(string name, string surname, int NumberOfPublishedBooks)
-            : this(name, surname)
+        internal Author(string name, string surname, int NumberOfPublishedBooks, DateTime dob)
+            : this(name, surname, dob)
         {
             this.NumberOfPublishedBooks = NumberOfPublishedBooks;
         }//ctor 2
