0145ac267d60d83fa13d3a909fca7ddfb5febd44
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-10-14 22:03:53 +0200

    Added a helper method for formating the collection of  authors to a single author's string

---
+++ BookInventory/Utilities/SQLServerAuthorsToString.cs
@@ -0,0 +1,21 @@
+﻿/*
+ *  This file contains the static class that will convert the list of author into a single author's string
+ */
+using System.Collections.Generic;
+
+namespace BookInventory.Utilities
+{
+    internal static class SQLServerAuthorsToString
+    {
+        public static string ToAuthorsString(IEnumerable<IAuthor> authors)
+        {
+            string _sAuthors = "";
+
+            //-Format the ID string the way that it is needed in the SQL Server database engine
+            foreach (IAuthor item in authors)
+                _sAuthors += item.ID + ",'" + item.Name + "','" + item.Surname + "','" + item.DOB.ToString("yyyy/MM/dd") + "';";
+
+            return _sAuthors;
+        }//ToAuthorsString
+    }//class
+}//namespace1
