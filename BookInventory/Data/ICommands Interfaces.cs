a6be565a54fb11cc0feb04a79081f0f16ebf4e18
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-10-14 21:02:14 +0200

    Added Command interfaces for the BookRepository implementations

---
+++ BookInventory/Data/ICommands Interfaces.cs
@@ -0,0 +1,31 @@
+﻿/*
+ *  This file contains all the different commnds that are used for the database CRUD operationss
+ */
+using System;
+using System.Collections.Generic;
+
+namespace BookInventory.Data
+{
+    public interface IAddBookCommand
+    {
+        bool AddBook(IBook book);
+    }//IAddBookCommand
+    public interface IRemoveBook
+    {
+        bool RemoveBook(IBook book);
+    }//IRemoveBook
+    public interface IUpdateBook
+    {
+        bool UpdateBook(IBook book);
+    }//IUpdateBook
+    public interface ILoadingBooks
+    {
+        IEnumerable<IBook> LoadAllBooks();
+        IBook FindBookByISBN(string isbn);
+    }//ILoadingBook
+    public interface IFilterBooks
+    {
+        IEnumerable<IBook> FilterBooks(Predicate<IBook> predicate);
+        IEnumerable<IBook> FilterBooks(string authorName, string genre, string title, int? release);
+    }//IFilterBooks
+}//namespace
