1bf1cc98ffe2c36cd95027565b4f52cb3b970f4b
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-10-15 12:09:22 +0200

    Impleamented the SQL Remove Command

--- BookInventory/Data/SQLServerDB/SQLServerCommands/SQLServerRemoveCommand.cs
+++ BookInventory/Data/SQLServerDB/SQLServerCommands/SQLServerRemoveCommand.cs
@@ -2,7 +2,8 @@
  *  This file contains the class that specifies the SQL Server Remove command
  */
 using System;
-
+using System.Data;
+using System.Data.SqlClient;
 namespace BookInventory
 {
     internal class SQLServerRemoveCommand : IRemoveBookCommand
@@ -14,7 +15,33 @@ namespace BookInventory
         }//ctor main
         public bool RemoveBook(IBook book)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (_dbService.GetConnection().State == ConnectionState.Closed)
+                    _dbService.GetConnection().Open();
+
+                //Create the sql command
+                SqlCommand cmd = new SqlCommand("pro_RemoveBook", (SqlConnection)_dbService.GetConnection());
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@BookID", book.ID);
+
+                int result = cmd.ExecuteNonQuery();
+
+                //If result is "1" then it was a successful transaction otherwise it failed
+                return result == 1;
+            }
+            catch
+            (Exception ex)
+            {
+                ExceptionLogger.GetLogger().LogError(ex);
+                return false;
+            }
+            finally
+            {
+                //Close the connection
+                if (_dbService.GetConnection().State == ConnectionState.Open)
+                    _dbService.GetConnection().Close();
+            }
         }//RemoveBook
     }//class
 }//namespace
