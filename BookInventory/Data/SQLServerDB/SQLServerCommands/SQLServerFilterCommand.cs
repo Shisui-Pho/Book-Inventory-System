1fefdedc13b8e90d6925a0e21290c76770e7e624
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-10-15 12:48:16 +0200

    Implemented the filtter command for the Sql command

--- BookInventory/Data/SQLServerDB/SQLServerCommands/SQLServerFilterCommand.cs
+++ BookInventory/Data/SQLServerDB/SQLServerCommands/SQLServerFilterCommand.cs
@@ -3,7 +3,9 @@
  */
 using System;
 using System.Collections.Generic;
-
+using System.Data;
+using System.Data.SqlClient;
+using BookInventory.Utilities;
 namespace BookInventory
 {
     internal class SQLServerFilterCommand : IFilterBooksCommand
@@ -16,12 +18,49 @@ namespace BookInventory
         public IEnumerable<IBook> FilterBooks(Predicate<IBook> predicate)
         {
             //We need to load all books
-            return default;
+            ILoadingBooksCommand cmd = new SQLLoadingCommand(_dbService);
+            IEnumerable<IBook> books = cmd.LoadAllBooks();
+
+            //Enumerate the books
+            foreach (IBook book in books)
+                if (predicate(book))
+                    yield return book;
         }//FilterBooks
 
         public IEnumerable<IBook> FilterBooks(bool matchAllCriteria, string authorName = null, string authorSurname = null, string genre = null, string title = null)
         {
-            throw new NotImplementedException();
+            IList<IBook> books = new List<IBook>(); 
+            try
+            {
+                //Create the command
+                SqlCommand cmd = new SqlCommand("proc_FilterBook", (SqlConnection)_dbService.GetConnection());
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                //Pass the parameters
+                cmd.Parameters.AddWithValue("@Book_Title ", title);
+                cmd.Parameters.AddWithValue("@Genre", genre);
+                cmd.Parameters.AddWithValue("@AuthorName", authorName);
+                cmd.Parameters.AddWithValue("@AuthorSurname", authorSurname);
+                cmd.Parameters.AddWithValue("@MustContainValues", matchAllCriteria);
+
+                //Execute command
+                SqlDataReader rd = cmd.ExecuteReader();
+
+                //Extract rows from the reader
+                books = SQLServerUtility.ReadToListOfBooks(rd);
+            }
+            catch
+            (Exception ex)
+            {
+                ExceptionLogger.GetLogger().LogError(ex);
+                books = new List<IBook>();
+            }
+            finally
+            {
+                if (_dbService.GetConnection().State == ConnectionState.Closed)
+                    _dbService.GetConnection().Close();
+            }
+            return books;
         }//FilterBooks
     }//class
 }//namespace
